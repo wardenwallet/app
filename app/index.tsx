2a589d946631be6902a1c34dc507cb8ca465ee3c
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-07-03 20:47:21 +0530

    trying biometric authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,34 @@
 import * as React from "react";
-import { View, ImageBackground } from "react-native";
+import { Alert, View } from "react-native";
 import { Text } from "~/components/ui/text";
-import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
-import { Fingerprint } from "lucide-react-native";
+
+import * as LocalAuthentication from "expo-local-authentication";
 
 export default function Screen() {
+  const [isBiometricSupported, setIsBiometricSupported] = React.useState(false);
+  React.useEffect(() => {
+    (async () => {
+      const compatible = await LocalAuthentication.hasHardwareAsync();
+      setIsBiometricSupported(compatible);
+    })();
+  });
+
+  const handleBiometricAuth = async () => {
+    try {
+      const biometricAuth = await LocalAuthentication.authenticateAsync({
+        promptMessage: "Login with Biometrics",
+        disableDeviceFallback: true,
+        cancelLabel: "Cancel",
+      });
+      if (biometricAuth.success) {
+        Alert.alert("Success", "You are in!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View className="flex-1 flex-col items-center max-h-fit">
       <View className="flex-1 flex justify-end px-6 bg-secondary/30 w-full">
@@ -13,30 +36,12 @@ export default function Screen() {
           Secure Self-Custody Wallet Solution
         </Text>
       </View>
-      <View className="flex-1 p-6 bg-secondary/30 w-full">
-        <Input
-          placeholder="email@example.com"
-          aria-labelledbyledBy="inputLabel"
-          aria-errormessage="inputError"
-          cursorColor="#ffffffCC"
-          selectionColor="#ffffff4D"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-          autoComplete="email"
-          autoCapitalize="none"
-          autoFocus={false}
-          className="rounded-xl mb-4 bg-secondary/30"
-        />
-        <Button className="rounded-xl mb-4" size="lg">
-          <Text>Continue</Text>
+      <View className="flex-1 justify-end p-6 pb-12 bg-secondary/30 w-full">
+        <Button className="mb-4" size="lg" onPress={handleBiometricAuth}>
+          <Text>Create New Wallet</Text>
         </Button>
-        <Button
-          variant="outline"
-          size="lg"
-          className="rounded-xl bg-secondary/30 flex flex-row items-center"
-        >
-          <Text>Continue with Passkey</Text>
-          <Fingerprint color="white" style={{ marginLeft: 8 }} size={16} />
+        <Button variant="outline" size="lg" className="bg-secondary/30">
+          <Text>Add Existing Wallet</Text>
         </Button>
       </View>
     </View>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { TextClassContext } from "~/components/ui/text";
 import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
-  "group flex items-center justify-center rounded-md web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2",
+  "group flex items-center justify-center rounded-xl web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2",
   {
     variants: {
       variant: {
@@ -14,14 +14,13 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent",
         secondary: "bg-secondary web:hover:opacity-80 active:opacity-80",
-        ghost:
-          "web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent",
+        ghost: "web:hover:bg-accent web:hover:text-accent-foreground",
         link: "web:underline-offset-4 web:hover:underline web:focus:underline ",
       },
       size: {
         default: "h-12 px-4 py-2 native:h-12 native:px-5 native:py-3",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8 native:h-14",
+        sm: "h-9 px-3",
+        lg: "h-11 px-8 native:h-14",
         icon: "h-10 w-10",
       },
     },
