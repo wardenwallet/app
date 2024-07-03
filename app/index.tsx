51c6e96cf5fae6f7f4d082cd1ef52e4ac8184d53
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-07-03 16:19:27 +0530

    fixed: minor android bugs

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, ImageBackground } from "react-native";
 import { Text } from "~/components/ui/text";
 import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
@@ -7,14 +7,10 @@ import { Fingerprint } from "lucide-react-native";
 
 export default function Screen() {
   return (
-    <View className="flex-1 flex-col items-center">
+    <View className="flex-1 flex-col items-center max-h-fit">
       <View className="flex-1 flex justify-end px-6 bg-secondary/30 w-full">
-        <Text className="text-3xl">
-          Warden —{" "}
-          <span className="text-primary/70">
-            Secure Self-Custody Wallet Solution
-          </span>
-          .
+        <Text className="text-4xl font-light">
+          Secure Self-Custody Wallet Solution
         </Text>
       </View>
       <View className="flex-1 p-6 bg-secondary/30 w-full">
@@ -22,15 +18,25 @@ export default function Screen() {
           placeholder="email@example.com"
           aria-labelledbyledBy="inputLabel"
           aria-errormessage="inputError"
+          cursorColor="#ffffffCC"
+          selectionColor="#ffffff4D"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          autoComplete="email"
+          autoCapitalize="none"
+          autoFocus={false}
           className="rounded-xl mb-4 bg-secondary/30"
         />
-        <Button className="rounded-xl mb-4">
+        <Button className="rounded-xl mb-4" size="lg">
           <Text>Continue</Text>
         </Button>
-        <Button variant="outline" className="rounded-xl bg-secondary/30">
-          <Text className="flex items-center">
-            Continue with Passkey <Fingerprint className="h-4 w-4 ml-2" />
-          </Text>
+        <Button
+          variant="outline"
+          size="lg"
+          className="rounded-xl bg-secondary/30 flex flex-row items-center"
+        >
+          <Text>Continue with Passkey</Text>
+          <Fingerprint color="white" style={{ marginLeft: 8 }} size={16} />
         </Button>
       </View>
     </View>
